394ec2ba453236eec5a1c4f16e0c476ca0314808
Anne Flinchbaugh <aflinchb@cs.utexas.edu>
2013-04-19 12:24:09 -0500

    added part 4 a

---
+++ testingStanford.scala
@@ -0,0 +1,18 @@
+import scala.io.Source
+
+
+val data = io.Source.fromFile(args(0)).getLines
+
+val dataset = <dataset>
+{for (line <- data)yield { val information = line.split(";;")
+				val polarity = if (information(0) == "0") "negative"
+						else if (information(0) == "2") "neutral"
+						else  "positive"    
+<item tweetid={information(1).toString} label={polarity.toString} target={information(3).toString} username={information(4).toString}>
+	<content>{information(5).toString}</content>
+	</item>
+}}
+</dataset>
+
+println(dataset)
+
