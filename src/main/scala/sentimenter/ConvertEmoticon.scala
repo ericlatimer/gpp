57a4aa6d4eb620ef0b1651d9544069767298b107
emoney <emoney33@gmail.com>
2013-04-20 10:38:44 -0500

    Done with probelm 5

--- src/main/scala/sentimenter/ConvertEmoticon.scala
+++ src/main/scala/sentimenter/ConvertEmoticon.scala
@@ -7,19 +7,20 @@ object ConvertEmoticon {
 
 	def main(args: Array[String]){
 		val files = new java.io.File(args(0)).listFiles.filter(_.getName.endsWith(".txt"))
-		files.foreach{ file => val data = io.Source.fromFile(file.getAbsolutePath()).getLines
 		val dataset = <dataset>
-{for (line <- data)yield { val information = line.split("\\t")
-				val polarity = if (file.getName == "sad.txt") "negative"
+		{
+		for(file <- files) yield { 
+					{for (line <- io.Source.fromFile(file.getAbsolutePath()).getLines) yield { val information = line.split("\\t")
+						val polarity = if (file.getName == "sad.txt") "negative"
 						else if (file.getName == "neutral.txt") "neutral"
 						else  "positive"    
 <item tweetid={information(0).toString} label={polarity.toString} target="unknown" username={information(1).toString}>
 	<content>{information(2).toString}</content>
 	</item>
 }}
+		
+		}}
 </dataset>
-
-		println(dataset)
-		}
+	println(dataset)
 	}
 }
